606468755a88fe9999b81c201de131a491accf94
iago <iagoaroucaferreira@hotmail.com>
2025-12-23 10:52:57 -0300

    feat(auth): add auth controller with register and login endpoints

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService,
+        private userService: UsersService
+    ) {}
+
+    @Post('login')
+    async login(@Body() body: any) {
+        return this.authService.login(body.email, body.password)
+    }
+}
