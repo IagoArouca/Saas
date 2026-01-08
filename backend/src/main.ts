64ad7b1bb306f815e7b477a018b6e57fd7f66b3d
iago <iagoaroucaferreira@hotmail.com>
2026-01-08 14:42:44 -0300

    ...

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      'https://saas-enqwhvbhp-iago-aroucas-projects.vercel.app', 
+      'saas-pied-mu.vercel.app',
+      'https://saas-iago-aroucas.vercel.app', 
+      'http://localhost:3000',
+      'http://localhost:5173'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 10000;
   
   await app.listen(port, '0.0.0.0');
   
